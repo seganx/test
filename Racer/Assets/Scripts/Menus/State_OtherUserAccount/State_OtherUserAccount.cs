82adad1ca2ede77d8485b1a23f745e04095217d8
mojtaba <mojtaba.rgb@gmail.com>
2019-08-16 00:39:42 +0430

    State_OtherUserAccount added.

---
+++ Racer/Assets/Scripts/Menus/State_OtherUserAccount/State_OtherUserAccount.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SeganX;
+
+public class State_OtherUserAccount : GameState
+{
+    [SerializeField] private RacerConfig[] racerConfigs;
+    [SerializeField] private Button nextToturialButton;
+    [SerializeField] private Button prevToturialButton;
+    int currentToturialPageIndex;
+
+    private void Start()
+    {
+        UiShowHide.ShowAll(transform);
+        UpdateRacer();
+
+        nextToturialButton.onClick.AddListener(() =>
+        {
+            currentToturialPageIndex++;
+            UpdateRacer();
+        });
+
+        prevToturialButton.onClick.AddListener(() =>
+        {
+            currentToturialPageIndex--;
+            UpdateRacer();
+        });
+    }
+
+    void UpdateRacer()
+    {
+        prevToturialButton.gameObject.SetActive(currentToturialPageIndex > 0);
+        nextToturialButton.gameObject.SetActive(currentToturialPageIndex < racerConfigs.Length - 1);
+    }
+}
