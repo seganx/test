180e81ec4377fe4ff56bba2e6f751831559f27b5
Sajad Beigjani <seganx@gmail.com>
2019-08-25 15:53:38 +0430

    improvements

--- Racer/Assets/Scripts/Menus/State_Playing/UiPlayingGesture.cs
+++ Racer/Assets/Scripts/Menus/State_Playing/UiPlayingGesture.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class UiPlayingGesture : MonoBehaviour, IEndDragHandler
+public class UiPlayingGesture : MonoBehaviour, IEndDragHandler, IDragHandler
 {
     [SerializeField] private Vector2 useNitrosDirection = new Vector2(5, 10);
 
     public static bool UseNitors { get; private set; }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
 
     public void OnEndDrag(PointerEventData eventData)
     {
--- Racer/Assets/Scripts/Menus/State_Playing/UiPlayingTutorial.cs
+++ Racer/Assets/Scripts/Menus/State_Playing/UiPlayingTutorial.cs
@@ -7,39 +7,39 @@ public class UiPlayingTutorial : MonoBehaviour
     [SerializeField] private GameObject[] tutorialObjects;
 
 
-    private void Start()
+    private IEnumerator Start()
     {
-        if (!GetPlayingTutorialShowed(0))
-            StartCoroutine(ShowThenHideTutorial(0, 5));
+        for (int i = 0; i < tutorialObjects.Length; i++)
+            tutorialObjects[i].SetActive(false);
 
-        if (!GetPlayingTutorialShowed(1))
-            StartCoroutine(ShowThenHideTutorial(1, 13));
+        if (RaceModel.IsTutorial)
+        {
+            yield return new WaitForSecondsRealtime(5);
+            tutorialObjects[0].SetActive(true);
 
-        if (!GetPlayingTutorialShowed(2))
-            StartCoroutine(ShowThenHideTutorial(2, 20));
+            yield return HoldTime();
 
-        if (GetPlayingTutorialShowed(0) && GetPlayingTutorialShowed(1) && GetPlayingTutorialShowed(2))
-            Destroy(gameObject);
+            yield return new WaitForSecondsRealtime(13);
+            tutorialObjects[1].SetActive(true);
+
+            yield return HoldTime();
+
+            yield return new WaitForSecondsRealtime(20);
+            tutorialObjects[2].SetActive(true);
+
+            yield return HoldTime();
+        }
+
+        Destroy(gameObject);
     }
 
-    private IEnumerator ShowThenHideTutorial(int index, float delay = 0)
+    private IEnumerator HoldTime()
     {
-        yield return new WaitForSeconds(delay);
-        tutorialObjects[index].SetActive(true);
-        SetPlayingTutorialShowed(index);
-        yield return new WaitForSeconds(6);
-        tutorialObjects[index].SetActive(false);
+        Time.timeScale = 0.1f;
+        yield return new WaitForSecondsRealtime(3);
+        Time.timeScale = 1;
 
-        if (index == 2) Destroy(gameObject);
-    }
-
-    private bool GetPlayingTutorialShowed(int index)
-    {
-        return PlayerPrefs.GetInt("PlayingTutorial_" + index, 0) > 0;
-    }
-
-    private void SetPlayingTutorialShowed(int index)
-    {
-        PlayerPrefs.SetInt("PlayingTutorial_" + index, 1);
+        for (int i = 0; i < tutorialObjects.Length; i++)
+            tutorialObjects[i].SetActive(false);
     }
 }
