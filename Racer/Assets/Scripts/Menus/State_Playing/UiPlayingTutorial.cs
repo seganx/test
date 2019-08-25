c31fe3bb1f20710a6b38f64e3c178f0f14600251
mojtaba <mojtaba.rgb@gmail.com>
2019-08-25 16:55:16 +0430

    UiPlayingTutorial updated.

--- Racer/Assets/Scripts/Menus/State_Playing/UiPlayingTutorial.cs
+++ Racer/Assets/Scripts/Menus/State_Playing/UiPlayingTutorial.cs
@@ -14,22 +14,22 @@ public class UiPlayingTutorial : MonoBehaviour
 
         if (RaceModel.IsTutorial)
         {
-            yield return new WaitForSecondsRealtime(5);
+            yield return new WaitForSecondsRealtime(4.5f);
             tutorialObjects[0].SetActive(true);
 
             yield return HoldTime();
 
-            yield return new WaitForSecondsRealtime(5);
+            yield return new WaitForSecondsRealtime(6);
             tutorialObjects[1].SetActive(true);
 
             yield return HoldTime();
 
-            yield return new WaitForSecondsRealtime(5);
+            yield return new WaitForSecondsRealtime(8);
             tutorialObjects[2].SetActive(true);
 
             yield return HoldTime();
 
-            yield return new WaitForSecondsRealtime(5);
+            yield return new WaitForSecondsRealtime(10);
             yield return new WaitUntil(() => PlayerPresenter.local.IsNitrosUsing);
             tutorialObjects[3].SetActive(true);
 
@@ -41,7 +41,7 @@ public class UiPlayingTutorial : MonoBehaviour
 
     private IEnumerator HoldTime()
     {
-        Time.timeScale = 0.1f;
+        Time.timeScale = 0.4f;
         yield return new WaitForSecondsRealtime(5);
         Time.timeScale = 1;
 
