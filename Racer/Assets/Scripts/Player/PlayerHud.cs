a9352b497d7ac043e47817cb4d5da6e6940f0c00
mojtaba <mojtaba.rgb@gmail.com>
2019-11-07 12:06:16 +0330

    GlobalConfig.asset updated.

--- Racer/Assets/Scripts/Player/PlayerHud.cs
+++ Racer/Assets/Scripts/Player/PlayerHud.cs
@@ -45,7 +45,7 @@ public class PlayerHud : MonoBehaviour
                 numberImage.sprite = positionSprites[player.player.CurrRank % positionSprites.Length];
                 var depth = transform.position.z - Camera.main.transform.position.z;
                 float fadeFrom = 3;
-                float fadeTo = 20;
+                float fadeTo = 15;
                 float fadeDis = (fadeTo - fadeFrom) * 0.5f;
                 box.alpha = Mathf.Clamp01(1 - Mathf.Pow((depth - fadeFrom - 1) / fadeDis - 1, 2));
                 box.transform.localScale = Vector3.one * Mathf.Lerp(0.002f, 0.015f, Mathf.Clamp01(depth / 50.0f));
